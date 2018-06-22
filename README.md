cffb73f584c2043e0eccd438a37a64131290ce0c
Bawaaaaah <limason51@gmail.com>
2018-06-22 09:08:48 +0200

    Working soft

---
+++ README.md
@@ -0,0 +1,10 @@
+# WAIT PID
+Software made in one night use to lock console by waiting one ore more PID (Process IDentifier)
+
+## How use it
+if you wan't wait the pid 1773
+`wait_pid 1773`
+if you wan't wait the pid 1773 and 3771
+`wait_pid 1773 3771`
+if you wan't wait the pid 1773 and 3771 n other PIDs
+`wait_pid 1773 3771 [PIDs...]`
---
+++ wait_pid/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wait_pid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pid = new int[args.Length];
+            for(int i = args.Length; i-- > 0;)
+            {
+                if (!int.TryParse(args[i], out pid[i]))
+                {
+                    throw (new Exception("INVALIDE PARAMETER DOES'NT SEEM TO BE A INTEGER => " + args[i]));
+                }
+                if(pid[i] <= 0)
+                    throw (new Exception("INVALIDE PARAMETER DOES'NT SEEM TO BE A GREATER THAN 0 PID=> " + args[i]));
+            }
+            Process p;
+            for (int i = pid.Length; i-- > 0;)
+            {
+                try
+                {
+                    p = Process.GetProcessById(pid[i]);
+                    p.WaitForExit();
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
